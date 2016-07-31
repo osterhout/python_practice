261db713ab7cc74e6d3a5d596795ca8c86dbfc39
osterhout <groovehard@gmail.com>
2016-07-31 13:28:04 -0800

    Update 03_lessthan10.py

--- 03_lessthan10.py
+++ 03_lessthan10.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# solution for http://www.practicepython.org/exercise/2014/02/26/04-divisors.html
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 b = int(raw_input("Pick a positive integer: "))
